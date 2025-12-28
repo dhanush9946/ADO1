0cf5ce9aa2c383227ae14904832c4f53729863cc
dhanush9946 <dhanushpoothanganam5@gmail.com>
2025-12-28 17:03:35 +0530

    connection

---
+++ ADO1Connection/Insert.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Microsoft.Data.SqlClient;
+
+namespace ADO1Connection
+{
+    internal class Insert
+    {
+        public static void Main1()
+        {
+            string cs = "Server=.;Database=TestDB;Trusted_Connection=True;Encrypt=True;TrustServerCertificate=True;";
+            using SqlConnection con = new SqlConnection(cs);
+            con.Open();
+            SqlCommand cmd = new SqlCommand("INSERT INTO Employees (Name) VALUES ('vijayasree')", con);
+            int row = cmd.ExecuteNonQuery();
+            Console.WriteLine(row + "rows affected");
+        }
+    }
+}
---
+++ ADO1Connection/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using ADO1Connection;
+using Microsoft.Data.SqlClient;
+
+class Program
+{
+    public static void Main()
+    {
+        //string cs = "Server=.;Database=TestDB;Trusted_Connection=True;Encrypt=True;TrustServerCertificate=True;";
+        //using (SqlConnection con=new SqlConnection(cs))
+        //{
+        //    con.Open();
+
+        //    SqlCommand cmd = new SqlCommand("Select Id,Name FROM Employees;", con);
+        //    SqlDataReader dr = cmd.ExecuteReader();
+
+        //    while (dr.Read())
+        //    {
+        //        Console.WriteLine(dr["Id"] + " " + dr["Name"]);
+        //    }
+
+        //}
+        Insert.Main1();
+    }
+}
