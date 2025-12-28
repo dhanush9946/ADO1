96807e413929969c34f37d0b6de07a23e14d6037
dhanush9946 <dhanushpoothanganam5@gmail.com>
2025-12-28 17:35:04 +0530

    Execute Scalar

---
+++ ADO1Connection/ExecuteSclar.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Microsoft.Data.SqlClient;
+
+namespace ADO1Connection
+{
+    internal class ExecuteSclar
+    {
+        public static void Main2()
+        {
+            string cs = "Server=.;Database=TestDB;Trusted_Connection=True;Encrypt=True;TrustServerCertificate=True;";
+            SqlConnection con = new SqlConnection(cs);
+            con.Open();
+            SqlCommand cmd = new SqlCommand("Select COUNT(*) from Employees", con);
+            int count = (int)cmd.ExecuteScalar();
+            Console.WriteLine(count);
+        }
+    }
+}
