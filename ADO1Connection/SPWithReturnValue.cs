62aa4185ad247b7dd1ec6f5ce2a53eb47cb518cc
dhanush9946 <dhanushpoothanganam5@gmail.com>
2025-12-29 21:30:48 +0530

    stored procedure with return value

---
+++ ADO1Connection/SPWithReturnValue.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.Data.SqlClient;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ADO1Connection
+{
+    internal class SPWithReturnValue
+    {
+        public static void Main8()
+        {
+            string cs = "Server=.;Database=TestDB;Trusted_Connection=True;Encrypt=True;TrustServerCertificate=True;";
+            using SqlConnection con = new SqlConnection(cs);
+            con.Open();
+
+            SqlCommand cmd = new SqlCommand("sp_AddEmployee", con);
+            cmd.CommandType = CommandType.StoredProcedure;
+
+            cmd.Parameters.AddWithValue("@Name", "hayyoda");
+            cmd.Parameters.AddWithValue("@Age", 19);
+
+            SqlParameter returnParameter = new SqlParameter();
+            returnParameter.Direction = ParameterDirection.ReturnValue;
+
+            cmd.Parameters.Add(returnParameter);
+            cmd.ExecuteNonQuery();
+
+            int result = (int)returnParameter.Value;
+            Console.WriteLine(result);
+        }
+    }
+}
