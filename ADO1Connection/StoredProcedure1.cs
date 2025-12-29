78f4216646a7a8172a7efdcf681e2ee1a48ffad9
dhanush9946 <dhanushpoothanganam5@gmail.com>
2025-12-29 17:06:17 +0530

    stored procedure

---
+++ ADO1Connection/StoredProcedure1.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.Data.SqlClient;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ADO1Connection
+{
+    internal class StoredProcedure1
+    {
+        public static void Main4()
+        {
+            string cs = "Server=.;Database=TestDB;Trusted_Connection=True;Encrypt=True;TrustServerCertificate=True;";
+            using SqlConnection con = new SqlConnection(cs);
+            con.Open();
+            SqlCommand cmd = new SqlCommand("sp_GetEmployees", con);
+
+            cmd.CommandType = CommandType.StoredProcedure;
+
+            SqlDataReader dr = cmd.ExecuteReader();
+
+            while (dr.Read())
+            {
+                Console.WriteLine(dr[0] + " " + dr[1] + " " + dr[2]);
+            }
+        }
+    }
+}
