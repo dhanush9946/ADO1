877d55b0c3f79ecdc4d17c4e9e2b0c336dbf5b9d
dhanush9946 <dhanushpoothanganam5@gmail.com>
2025-12-29 16:11:44 +0530

    Altering table

---
+++ ADO1Connection/Altering.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.Data.SqlClient;
+
+namespace ADO1Connection
+{
+    internal class Altering
+    {
+        public static void Main3()
+        {
+
+
+            string cs = "Server=.;Database=TestDB;Trusted_Connection=True;Encrypt=True;TrustServerCertificate=True;";
+            using SqlConnection con = new SqlConnection(cs);
+            con.Open();
+            SqlCommand cmd = new SqlCommand("Alter table Employees add Age INT", con);
+            int rows = cmd.ExecuteNonQuery();
+            Console.WriteLine(rows + "affected");
+
+        }
+    }
+}
