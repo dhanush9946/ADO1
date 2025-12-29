d58be462ced7645abaedeb2db7e40ea94fabc4af
dhanush9946 <dhanushpoothanganam5@gmail.com>
2025-12-29 20:07:10 +0530

    stored procedure with parameter

---
+++ ADO1Connection/StoredProcedureWithParameter.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.Data.SqlClient;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ADO1Connection
+{
+    internal class StoredProcedureWithParameter
+    {
+        public static void Main6()
+        {
+            string cs = "Server=.;Database=TestDB;Trusted_Connection=True;Encrypt=True;TrustServerCertificate=True;";
+            using SqlConnection con = new SqlConnection(cs);
+            con.Open();
+            SqlCommand cmd = new SqlCommand("sp_GetEmployeeById", con);
+            cmd.CommandType = CommandType.StoredProcedure;
+            cmd.Parameters.AddWithValue("@Id", 1);
+
+            SqlDataReader dr = cmd.ExecuteReader();
+
+            while (dr.Read())
+            {
+                Console.WriteLine(dr["Name"]+" "+ dr["Age"]);
+            }
+
+        }
+    }
+}
