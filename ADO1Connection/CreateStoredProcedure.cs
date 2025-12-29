61ba1f322ca473d34204c38b2208fbd752893a1f
dhanush9946 <dhanushpoothanganam5@gmail.com>
2025-12-29 18:09:44 +0530

    stored procedure created using ADO.NET,inside the console

---
+++ ADO1Connection/CreateStoredProcedure.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Microsoft.Data.SqlClient;
+
+namespace ADO1Connection
+{
+    internal class CreateStoredProcedure
+    {
+        public static void Main5()
+        {
+            string cs = "Server=.;Database=TestDB;Trusted_Connection=True;Encrypt=True;TrustServerCertificate=True;";
+            using SqlConnection con = new SqlConnection(cs);
+            con.Open();
+            SqlCommand cmd = new SqlCommand("CREATE PROCEDURE sp_GetName as begin select Name from Employees end", con);
+            cmd.ExecuteNonQuery();
+            Console.WriteLine("sp created successfully");
+        }
+    }
+}
