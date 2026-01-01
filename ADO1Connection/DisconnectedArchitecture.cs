d943c0ea81dd187204a2abf0a5f07c89f55e44e1
dhanush9946 <dhanushpoothanganam5@gmail.com>
2026-01-01 21:34:19 +0530

    Disconnected Architecture

---
+++ ADO1Connection/DisconnectedArchitecture.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.Data.SqlClient;
+using System.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ADO1Connection
+{
+    internal class DisconnectedArchitecture
+    {
+        public static void Main9()
+        {
+            string cs = "Server=.;Database=TestDB;Trusted_Connection=True;Encrypt=True;TrustServerCertificate=True;";
+            using SqlConnection con = new SqlConnection(cs);
+
+            SqlDataAdapter da = new SqlDataAdapter("Select * from Employees", con);
+
+            DataSet ds = new DataSet();
+            da.Fill(ds, "Employees");
+
+            foreach(DataRow row in ds.Tables["Employees"].Rows)
+            {
+                Console.WriteLine(row["Name"] + "_" + row["Age"]);
+            }
+        }
+    }
+}
