6d3efb0a36a8c9af89dfc67921c3cde056c0f9dd
dhanush9946 <dhanushpoothanganam5@gmail.com>
2026-01-02 17:12:34 +0530

    Multiple queries

---
+++ ADO1Connection/MultipleQueries.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Data;
+using Microsoft.Data.SqlClient;
+
+namespace ADO1Connection
+{
+    internal class MultipleQueries
+    {
+        public static void Main14()
+        {
+            string cs = "Server=.;Database=TestDB;Trusted_Connection=True;Encrypt=True;TrustServerCertificate=True;";
+            using SqlConnection con = new SqlConnection(cs);
+            con.Open();
+            SqlCommand cmd = new SqlCommand("Select * from Employees;Select * from Students;", con);
+
+            SqlDataReader dr = cmd.ExecuteReader();
+
+            while (dr.Read())
+            {
+                Console.WriteLine(dr["Name"]);
+            }
+            dr.NextResult();
+            while (dr.Read())
+            {
+                Console.WriteLine(dr["FirstName"]);
+            }
+
+        }
+    }
+}
