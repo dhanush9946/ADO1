62f54887a9702e189e595d494033c1570dae3d60
dhanush9946 <dhanushpoothanganam5@gmail.com>
2026-01-02 23:53:05 +0530

    DataView filtering

---
+++ ADO1Connection/Dataview.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Data;
+using Microsoft.Data.SqlClient;
+
+namespace ADO1Connection
+{
+    internal class Dataview
+    {
+        public static void Main15()
+        {
+            string cs = "Server=.;Database=TestDB;Trusted_Connection=True;Encrypt=True;TrustServerCertificate=True;";
+            using SqlConnection con = new SqlConnection(cs);
+
+            SqlDataAdapter da = new SqlDataAdapter("SELECT * FROM Employees", con);
+            DataTable dt = new DataTable();
+            da.Fill(dt);
+
+            DataView dv = new DataView(dt);
+            dv.Sort = "Age DESC";
+            
+            foreach(DataRowView row in dv)
+            {
+                foreach(var item in row.Row.ItemArray)
+                {
+                    Console.WriteLine(item + "\t");
+                }
+                Console.WriteLine();
+            }
+
+
+        }
+    }
+}
