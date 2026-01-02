47bf22ebd2596db1e17ba5cee8814d9f42dd37ea
dhanush9946 <dhanushpoothanganam5@gmail.com>
2026-01-02 12:54:48 +0530

    Updated existing row

---
+++ ADO1Connection/UpdateDataset.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Data;
+using Microsoft.Data.SqlClient;
+
+namespace ADO1Connection
+{
+    internal class UpdateDataset
+    {
+        public static void Main11()
+        {
+            string cs = "Server=.;Database=TestDB;Trusted_Connection=True;Encrypt=True;TrustServerCertificate=True;";
+            SqlConnection con = new SqlConnection(cs);
+
+            SqlDataAdapter da = new SqlDataAdapter("SELECT * FROM Employees", con);
+            DataSet ds = new DataSet();
+            da.Fill(ds, "Employees");
+
+            DataRow row = ds.Tables["Employees"].Rows[0];
+            row["Age"] = 30;
+
+            DataRow roww = ds.Tables["Employees"].Rows[0];
+            Console.WriteLine(roww["Name"] + "-----" + roww["Age"]);
+        }
+    }
+}
