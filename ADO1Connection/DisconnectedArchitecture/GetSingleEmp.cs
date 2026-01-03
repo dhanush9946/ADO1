879a3fd0b69dac9691f4397482dab244104f4c36
dhanush9946 <dhanushpoothanganam5@gmail.com>
2026-01-03 18:51:58 +0530

    sp with id Disconnected arc

---
+++ ADO1Connection/DisconnectedArchitecture/GetSingleEmp.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.Data.SqlClient;
+using System.Data;
+
+
+namespace ADO1Connection
+{
+    internal class GetSingleEmp
+    {
+        public static void Main22()
+        {
+            string cs = "Server=.;Database=TestDB;Trusted_Connection=True;Encrypt=True;TrustServerCertificate=True;";
+            using SqlConnection con = new SqlConnection(cs);
+
+            SqlDataAdapter da = new SqlDataAdapter("GetById", con);
+            da.SelectCommand.CommandType = CommandType.StoredProcedure;
+
+            da.InsertCommand.Parameters.AddWithValue("@Id", 2);
+
+            DataSet ds = new DataSet();
+            da.Fill(ds);
+
+            foreach(DataRow row in ds.Tables[0].Rows)
+            {
+                Console.WriteLine(row["Name"]);
+            }
+        }
+    }
+}
