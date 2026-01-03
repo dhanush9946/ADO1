3e3f1bfd2d2c1130551a8b35ea6c92aff6f26138
dhanush9946 <dhanushpoothanganam5@gmail.com>
2026-01-03 15:20:37 +0530

    Stored procedure with disconnected

---
+++ ADO1Connection/ConnectedArchitecture/SPGetById.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Data;
+using Microsoft.Data.SqlClient;
+
+namespace ADO1Connection.ConnectedArchitecture
+{
+    internal class SPGetById
+    {
+        public static void Main18()
+        {
+            string cs = "Server=.;Database=TestDB;Trusted_Connection=True;Encrypt=True;TrustServerCertificate=True;";
+            using SqlConnection con = new SqlConnection(cs);
+            con.Open();
+            SqlCommand cmd = new SqlCommand("GetById", con);
+            cmd.CommandType = CommandType.StoredProcedure;
+
+            cmd.Parameters.AddWithValue("@Id", 1);
+
+            SqlDataReader dr = cmd.ExecuteReader();
+
+            while (dr.Read()) { Console.WriteLine(dr["Name"]); }
+        }
+    }
+}
